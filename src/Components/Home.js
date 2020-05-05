dbf9f143b747148f88782c174e7c10b1449a46cd
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-05-04 22:34:27 -0400

    MVP completed except testing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
+import icon from '../Pizza.jpg'
 
-const Home = () => {
-    return(
-        <div>
-            Home Page
-        </div>
+const HomePage = () => {
+
+    return (
+        <nav>
+
+            <h1>Lambda Eats The Best Pizza in Town</h1>
+            <img src={icon}/>
+            
+
+        </nav>
+
     )
 }
 
-export default Home;
+export default HomePage;
